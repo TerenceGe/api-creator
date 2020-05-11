ae9418e7ba569a819c09f00b443ae3f8082b624c
Terence Ge <Terencegehui@yahoo.com>
2020-05-11 11:26:49 +0800

    change readme

--- webpack.config.js
+++ webpack.config.js
@@ -52,10 +52,40 @@ const browserConfig = {
   },
   output: {
     ...baseConfig.output,
-    filename: 'index.js',
+    filename: 'browser.js',
     libraryTarget: 'umd',
     publicPath: '/'
   }
 }
 
-module.exports = browserConfig
+const nodeConfig = {
+  ...baseConfig,
+  target: 'node',
+  context: resolve('src'),
+  devtool: false,
+  entry: './index.js',
+  externals: {
+    'cross-fetch': 'cross-fetch'
+  },
+  output: {
+    ...baseConfig.output,
+    filename: 'node.js',
+    libraryTarget: 'commonjs2',
+    publicPath: '/'
+  },
+  node: {
+    console: false,
+    global: false,
+    process: false,
+    Buffer: false,
+    __filename: false,
+    __dirname: false
+  }
+}
+
+const configs = {
+  browser: browserConfig,
+  node: nodeConfig
+}
+
+module.exports = configs[process.env.TARGET]
